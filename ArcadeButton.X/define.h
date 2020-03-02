3ca84bbe0e86b15f13771fb3c26976527a69dc05
長沢良和 <nagasawayoshikazu@nagasawayoshikazunoMacBook-puro.local>
2020-03-02 18:34:15 +0900

    初期化部分の追加

--- ArcadeButton.X/define.h
+++ ArcadeButton.X/define.h
@@ -3,12 +3,15 @@
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 
-#define PORT_BUTTON RA0 // dummy
-#define PORT_LED_R  RA1 // dummy
-#define PORT_LED_G  RA2 // dummy
-#define PORT_LED_B  RA3 // dummy
+#define PORT_BUTTON RA3 // ボタン入力
+#define PORT_LED_R  RA2 // LED 赤出力
+#define PORT_LED_G  RA4 // LED 緑出力
+#define PORT_LED_B  RA5 // LED 青出力
+
+#define _XTAL_FREQ 8000000
 
 const uint8_t LED_MAX         = 8;
+
 
 #endif	/* DEFINE_H */
 
--- ArcadeButton.X/settings.c
+++ ArcadeButton.X/settings.c
@@ -31,10 +31,45 @@
 
 void initSystem()
 {
+    OSCCON = 0b11110000;
+
     // ポートの I/O初期化
+    TRISA  = 0b00110110;
+    ANSELA = 0x00; // All pins digital
+    ADCON0 = 0x00; // Disable ADC
+    CM1CON0bits.C1ON = 0; // Disable Comparator
 }
 
 void initSerial()
 {
     // UART の初期化
+    TRISA0 = 0; // Set RA0 as o/p pin.
+    TRISA1 = 1; // Set RA1 as i/p pin.
+    PIR1bits.RCIF = 0; // Clear UART receive flag.
+    PIR1bits.TXIF = 0; // Clear UART transmit flag.
+    // SPBRGH = 17 >> 8; // For 9600 Baud and with 8 Mhz Crystal
+    // SPBRGL = 17 & 0xFF;
+    SPBRG = 17;
+    APFCONbits.RXDTSEL = 0; // RA1 as RX Pin
+    APFCONbits.TXCKSEL = 0; // RA0 as TX Pin
+    WPUAbits.WPUA0 = 1; // Enable weak pull up on RA0
+    WPUAbits.WPUA1 = 1; // Enable weak pull up on RA1
+    TXSTAbits.TX9= 0; // 8-BIT DATA MODE
+    TXSTAbits.TXEN = 1; // ENABLE TRANSMITTER
+    TXSTAbits.SYNC = 0; // ENABLE ASYNCHRONOUS MODE
+    TXSTAbits.SENDB = 1; // SYNC BREAK TRANSMISSION COMPLETED
+    TXSTAbits.BRGH =0; // SELECT HIGH BAUD RATE
+    BAUDCONbits.SCKP = 0; // DON'T INVERT POLARITY
+    BAUDCONbits.BRG16 = 0; // 8-BIT BAUDRATE GENERATOR
+    BAUDCONbits.ABDEN = 0; // AUTO BAUD RATE DETECT DISABLE
+    RCSTAbits.RX9 = 0; // ENABLE 8-BIT RECEPTION
+    RCSTAbits.CREN = 1; // ENABLE RECEIVER
+    RCSTAbits.FERR = 0;
+    RCSTAbits.OERR = 0;
+    RCSTAbits.SPEN = 1; // ENABLE SERIAL PORT
+    PIE1bits.RCIE = 1; // Enable USART RX Interrupt. 
+
+    INTCON = 0x0; // Initialize interrupt register.
+    INTCONbits.PEIE = 1; // Enable Peripheral Interrupt
+    INTCONbits.GIE = 1; // Enable Global Interrupt
 }
